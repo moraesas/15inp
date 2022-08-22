d834085e761178da226d77d7f0744bb16dc5a38a
moraesas <106272691+moraesas@users.noreply.github.com>
2022-08-22 20:24:05 -0300

    Create 1.11.py

---
+++ 1.11.py
@@ -0,0 +1,5 @@
+km = float(input('digite a quantidade de km percorridos: '))
+dias = int(input('digite a quantidade de dias alugados: '))
+
+preço = (60 * dias) + (0.15 * km)
+print(' o total a ser pago é :' + str(preço))
