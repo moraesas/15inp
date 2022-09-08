61f31c87fead9bfc393294d8065b10310a7135f1
moraesas <106272691+moraesas@users.noreply.github.com>
2022-09-08 08:42:17 -0300

    Update 1.7.py

--- 1.7.py
+++ 1.7.py
@@ -1,7 +1,19 @@
-import sys 
+import sys
+dias = int(sys.argv[1])
+horas = int(sys.argv[2])
+minutos = int(sys.argv[3])
+segundos = int(sys.argv[4])
 
-dias = sys.argv[1]
-horas = sys.argv[2]
-minutos =  sys.argv[3]
-segundos = sys.argv[4]
 
+# 1 dia = 24h
+# 24h = 86400
+# 1h = 3600
+# 1 minuto = 60
+
+dias = dias * 24
+horas = horas * 3600
+minutos = minutos * 60
+
+total = dias + horas + minutos + segundos
+
+print('o total em segundos do usuario é %.2f' %total)
