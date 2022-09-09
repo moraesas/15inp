0bc27af6f73106fafc011b7dd96157b5bfb602c4
moraesas <106272691+moraesas@users.noreply.github.com>
2022-09-09 09:59:14 -0300

    Update 2.4(aumento_salario_faixa).py

--- 2.4(aumento_salario_faixa).py
+++ 2.4(aumento_salario_faixa).py
@@ -1,12 +1,12 @@
 salario = float(input('Digite seu salário: '))
 aumento = 0
-if salario > 1250 or salario >= 1250:
+if salario <= 1250:
     aumento= salario * (15/100)
     salario = float(salario + aumento)
     print('O aumento foi de R$',aumento, ' novo salário é de R$%.2f' %salario)
 
 
-elif salario > 1250:
+else:
     aumento = salario * (10/100)
     salario = float(salario + aumento)
     print('O aumento foi de RS',aumento,'novo salário é de R$%.2f' %salario)
